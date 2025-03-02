576ab1403b828919ff36568c94df7a44efb55ab9
HongThien1304 <thienpt2004@gmail.com>
2025-03-02 15:12:53 +0700

    ex02

---
+++ lab-01/ex02/bai01.py
@@ -0,0 +1,5 @@
+name = input("huu nhan")
+age = int(input("21 "))  # Convert input to integer
+
+print("Hello, " + name + "!")
+print("You are " + str(age) + " years old.") # Convert age to string for printing
