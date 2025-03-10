80e2ee5c66a5a7fbd9e15ebca5de377b0dc77252
HongThien1304 <thienpt2004@gmail.com>
2025-03-10 14:39:14 +0700

    add lab02 transpotition

--- lab-02/api.py
+++ lab-02/api.py
@@ -3,6 +3,7 @@ from cipher.caesar import CaesarCipher
 from cipher.vigenere import VigenereCipher
 from cipher.railfence import RailFenceCipher
 from cipher.Playfair import PlayFairCipher
+from cipher.transpostition import TranspositionCipher
 app = Flask(__name__)
 
 #CAESAR CIPHER ALGORITHM
@@ -93,9 +94,24 @@ def playfair_decrypt():
 
     return jsonify({'decrypted_text': decrypted_text})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#TRANSPOSITION CIPHER ALGORITHM
+transposition_cipher = TranspositionCipher()
 
+@app.route('/api/transposition/encrypt', methods=['POST'])
+def transposition_encrypt():
+    data = request.get_json()
+    plain_text = data.get('plain_text')
+    key = int(data.get('key'))
+    encrypted_text = transposition_cipher.encrypt(plain_text, key)
+    return jsonify({'encrypted_text': encrypted_text})
+
+@app.route('/api/transposition/decrypt', methods=['POST'])
+def transposition_decrypt():
+    data = request.get_json()
+    cipher_text = data.get('cipher_text')
+    key = int(data.get('key'))
+    decrypted_text = transposition_cipher.decrypt(cipher_text, key)
+    return jsonify({'decrypted_text': decrypted_text})
 
 #main function
 if __name__ == "__main__":
