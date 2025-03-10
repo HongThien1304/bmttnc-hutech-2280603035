378aaddb07a9d07d270e0204cb355176ba33beeb
HongThien1304 <thienpt2004@gmail.com>
2025-03-10 12:44:53 +0700

    Delete lab-01 directory

--- lab-01/ex02/bai01.py
+++
@@ -1,5 +0,0 @@
-name = input("huu nhan")
-age = int(input("21 "))  # Convert input to integer
-
-print("Hello, " + name + "!")
-print("You are " + str(age) + " years old.") # Convert age to string for printing
